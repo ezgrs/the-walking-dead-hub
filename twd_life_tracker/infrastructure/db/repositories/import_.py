303c173714f4e67067b88cddd9218c4d95bf98f1
Enzo Santos <enzo.santos29@hotmail.com>
2026-04-11 16:49:23 -0300

    fix: allows multiple imports without duplicating data

--- twd_life_tracker/infrastructure/db/repositories/import_.py
+++ twd_life_tracker/infrastructure/db/repositories/import_.py
@@ -19,6 +19,73 @@ from twd_life_tracker.infrastructure.db.models.appearance_form import (
 )
 
 
+class Upsert[
+    Model: sqlmodel.SQLModel,
+    Id: object,
+    Key,
+]:
+    @staticmethod
+    async def of[
+        M: sqlmodel.SQLModel,
+        I: object,
+        K,
+    ](
+        session: sqlmodel.ext.asyncio.session.AsyncSession,
+        model: type[M],
+        *,
+        on_id: typing.Callable[[M], I | None],
+        on_key: typing.Callable[[M], K],
+    ) -> "Upsert[M, I, K]":
+        return Upsert(
+            session=session,
+            model=model,
+            mapping={
+                on_key(model): model
+                for model in await session.exec(sqlmodel.select(model))
+            },
+            on_id=on_id,
+            on_key=on_key,
+        )
+
+    session: sqlmodel.ext.asyncio.session.AsyncSession
+    model: type[Model]
+    on_id: typing.Callable[[Model], Id | None]
+    on_key: typing.Callable[[Model], Key]
+    mapping: dict[Key, Model]
+
+    def __init__(
+        self,
+        *,
+        session: sqlmodel.ext.asyncio.session.AsyncSession,
+        model: type[Model],
+        mapping: dict[Key, Model],
+        on_id: typing.Callable[[Model], Id | None],
+        on_key: typing.Callable[[Model], Key],
+    ) -> None:
+        self.session = session
+        self.model = model
+        self.mapping = mapping
+        self.on_id = on_id
+        self.on_key = on_key
+
+    async def get_or_insert(
+        self,
+        key: Key,
+        *,
+        on_insert: typing.Callable[[], Model],
+    ) -> tuple[Id, Model]:
+        model = self.mapping.get(key)
+        if model is None:
+            model = on_insert()
+            self.session.add(model)
+            await self.session.flush()
+            self.mapping[key] = model
+
+        model_id = self.on_id(model)
+        assert model_id is not None
+        return model_id, model
+
+
 class ImportRepository(BaseImportRepository):
     session: sqlmodel.ext.asyncio.session.AsyncSession
 
@@ -34,69 +101,93 @@ class ImportRepository(BaseImportRepository):
         page_loader: PageLoader,
         tag_parser: TagParser[EpisodePage],
     ) -> None:
-        entities_mapping: dict[str, EntityModel] = {}
+        episode_upsert = await Upsert.of(
+            self.session,
+            EpisodeModel,
+            on_id=lambda episode: episode.id,
+            on_key=lambda episode: episode.wiki_href,
+        )
+        entity_upsert = await Upsert.of(
+            self.session,
+            EntityModel,
+            on_id=lambda entity: entity.id,
+            on_key=lambda entity: entity.wiki_href,
+        )
+        appearance_upsert = await Upsert.of(
+            self.session,
+            AppearanceModel,
+            on_id=lambda appearance: appearance.id,
+            on_key=lambda appearance: (
+                appearance.entity_id,
+                appearance.episode_id,
+            ),
+        )
+        appearance_form_upsert = await Upsert.of(
+            self.session,
+            AppearanceFormModel,
+            on_id=lambda appearance_form: appearance_form.id,
+            on_key=lambda appearance_form: appearance_form.appearance_id,
+        )
+
         async with page_loader:
-            episode: EpisodePage | None = None
-            while episode is None or (
-                episode.season_number,
-                episode.episode_number,
+            current_episode: EpisodePage | None = None
+            while current_episode is None or (
+                current_episode.season_number,
+                current_episode.episode_number,
             ) < (7, 16):
                 href: str
-                if episode is None:
+                if current_episode is None:
                     href = "/wiki/Days_Gone_Bye_(TV_Series)"
                 else:
-                    href = episode.next_page_href
+                    href = current_episode.next_page_href
 
                 episode_page_tag = await page_loader.load(href)
                 episode = await tag_parser.parse(episode_page_tag)
-                episode_model = EpisodeModel(
-                    id=None,
-                    name=episode.title,
-                    wiki_href=episode.href,
-                    season_number=episode.season_number,
-                    episode_number=episode.episode_number,
+                episode_model_id, _ = await episode_upsert.get_or_insert(
+                    href,
+                    on_insert=lambda: EpisodeModel(
+                        id=None,
+                        name=episode.title,
+                        wiki_href=episode.href,
+                        season_number=episode.season_number,
+                        episode_number=episode.episode_number,
+                    ),
                 )
-                self.session.add(episode_model)
-                await self.session.flush()
-
-                episode_model_id = episode_model.id
-                assert episode_model_id is not None
 
                 for entity_appearance in episode.entity_appearances:
                     entity_page_href = entity_appearance.entity_page_href
-                    entity_model = entities_mapping.get(entity_page_href)
-                    if entity_model is None:
-                        entity_model = EntityModel(
+                    entity_model_id, _ = await entity_upsert.get_or_insert(
+                        entity_page_href,
+                        on_insert=lambda: EntityModel(
                             id=None,
                             name=entity_appearance.entity_name,
                             wiki_href=entity_page_href,
-                        )
-                        self.session.add(entity_model)
-                        await self.session.flush()
-
-                    entity_model_id = entity_model.id
-                    assert entity_model_id is not None
-
-                    appearance_model = AppearanceModel(
-                        id=None,
-                        episode_id=episode_model_id,
-                        entity_id=entity_model_id,
-                        type_id=entity_appearance.appearance_type_id,
+                        ),
                     )
-                    self.session.add(appearance_model)
-                    await self.session.flush()
 
-                    appearance_model_id = appearance_model.id
-                    assert appearance_model_id is not None
+                    appearance_model_id, _ = (
+                        await appearance_upsert.get_or_insert(
+                            (episode_model_id, entity_model_id),
+                            on_insert=lambda: AppearanceModel(
+                                id=None,
+                                episode_id=episode_model_id,
+                                entity_id=entity_model_id,
+                                type_id=entity_appearance.appearance_type_id,
+                            ),
+                        )
+                    )
 
                     for (
                         appearance_form_type_id
                     ) in entity_appearance.appearance_form_types_ids:
-                        self.session.add(
-                            AppearanceFormModel(
+                        await appearance_form_upsert.get_or_insert(
+                            appearance_model_id,
+                            on_insert=lambda: AppearanceFormModel(
                                 id=None,
                                 appearance_id=appearance_model_id,
                                 type_id=appearance_form_type_id,
-                            )
+                            ),
                         )
+
+                current_episode = episode
         await self.session.commit()
