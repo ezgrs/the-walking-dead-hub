3d71b64a5929a5abc70c0cfa48837bb419af3c1b
Enzo Santos <enzo.santos29@hotmail.com>
2026-04-12 14:27:18 -0300

    [b] feat: rewrite __main__.py using redis

--- backend/twd_life_tracker/__main__.py
+++ backend/twd_life_tracker/__main__.py
@@ -1,9 +1,11 @@
 import asyncio
 
 import aiopath
+import redis.asyncio
 import sqlmodel
 import sqlmodel.ext.asyncio.session
 
+from twd_life_tracker.domain.services.episode_page_loader import EpisodeLoader
 from twd_life_tracker.domain.services.page_loader import PageLoader
 from twd_life_tracker.domain.services.tag_parser import TagParser
 from twd_life_tracker.infrastructure.db.repositories.alias import (
@@ -19,14 +21,19 @@ from twd_life_tracker.domain.models.settings import Settings
 async def main() -> None:
     settings = Settings()  # pyright: ignore[reportCallIssue]
 
+    r = redis.asyncio.Redis(
+        host=settings.redis_host,
+        port=settings.redis_port,
+        username=settings.redis_username,
+        password=settings.redis_password,
+        db=settings.redis_name,
+    )
+
     db_engine = create_engine(settings)
     async with sqlmodel.ext.asyncio.session.AsyncSession(
         db_engine,
         expire_on_commit=False,
     ) as session:
-        import_repository = ImportRepository(session)
-        alias_repository = AliasRepository(session)
-
         page_loader = PageLoader.from_server()
         cache_dir_path = settings.cache_dir_path
         if cache_dir_path is not None:
@@ -35,15 +42,21 @@ async def main() -> None:
                 dpath
             ).with_write_through_cache(dpath)
 
-        tag_parser = TagParser.episode_page(
-            entity_appearance_parser=TagParser.entity_appearance(
-                alias_repository=alias_repository,
-            ),
+        episode_loader = (
+            EpisodeLoader.from_server(
+                page_loader=page_loader,
+                tag_parser=TagParser.episode_page(
+                    entity_appearance_parser=TagParser.entity_appearance(
+                        alias_repository=AliasRepository(session),
+                    ),
+                ),
+            )
+            .with_write_through_cache(r)
+            .with_read_through_cache(r)
         )
-        await import_repository.import_data(
-            page_loader=page_loader,
-            tag_parser=tag_parser,
-        )
+
+        async with page_loader:
+            await ImportRepository(session).import_data(loader=episode_loader)
 
 
 if __name__ == "__main__":
