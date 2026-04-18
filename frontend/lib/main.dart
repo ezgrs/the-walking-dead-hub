06105cecddfcdf90b66c73726307eac3dcf07469
Enzo Santos <enzo.santos29@hotmail.com>
2026-04-18 16:24:40 -0300

    [f] feat: add support to change locale dynamically

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -28,63 +28,84 @@ class AppSpacing {
   const AppSpacing._();
 }
 
+class LocaleController {
+  final ValueNotifier<Locale> locale = ValueNotifier(const Locale('en', 'US'));
+
+  void setLocale(Locale newLocale) {
+    locale.value = newLocale;
+  }
+}
+
+final LocaleController localeController = LocaleController();
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'The Walking Dead Hub',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        inputDecorationTheme: const InputDecorationTheme(
-          border: OutlineInputBorder(),
-        ),
-        fontFamily: "JollyLodger",
-        scrollbarTheme: ScrollbarThemeData(
-          thickness: WidgetStateProperty.all(10),
-          radius: Radius.zero,
-          thumbColor: WidgetStateProperty.all(Colors.grey),
-          thumbVisibility: WidgetStateProperty.all(true),
-        ),
-      ),
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        ...GlobalMaterialLocalizations.delegates,
-      ],
-      builder: (context, child) {
-        child = rf.ResponsiveBreakpoints.builder(
-          child: child!,
-          breakpoints: const [
-            rf.Breakpoint(start: 0, end: 319),
-            rf.Breakpoint(start: 320, end: 719, name: kDeviceMobile),
-            rf.Breakpoint(start: 720, end: 1279, name: kDeviceTablet),
-            rf.Breakpoint(start: 1280, end: 1600, name: kDeviceLaptop),
-            rf.Breakpoint(
-              start: 1601,
-              end: double.infinity,
-              name: kDeviceDesktop,
+    return ValueListenableBuilder(
+      valueListenable: localeController.locale,
+      builder: (context, locale, _) {
+        return MaterialApp.router(
+          title: 'The Walking Dead Hub',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            inputDecorationTheme: const InputDecorationTheme(
+              border: OutlineInputBorder(),
             ),
+            fontFamily: "JollyLodger",
+            scrollbarTheme: ScrollbarThemeData(
+              thickness: WidgetStateProperty.all(10),
+              radius: Radius.zero,
+              thumbColor: WidgetStateProperty.all(Colors.grey),
+              thumbVisibility: WidgetStateProperty.all(true),
+            ),
+          ),
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            ...GlobalMaterialLocalizations.delegates,
           ],
+          builder: (context, child) {
+            child = rf.ResponsiveBreakpoints.builder(
+              child: child!,
+              breakpoints: const [
+                rf.Breakpoint(start: 0, end: 319),
+                rf.Breakpoint(start: 320, end: 719, name: kDeviceMobile),
+                rf.Breakpoint(start: 720, end: 1279, name: kDeviceTablet),
+                rf.Breakpoint(start: 1280, end: 1600, name: kDeviceLaptop),
+                rf.Breakpoint(
+                  start: 1601,
+                  end: double.infinity,
+                  name: kDeviceDesktop,
+                ),
+              ],
+            );
+            final MediaQueryData media = MediaQuery.of(context);
+            child = MediaQuery(
+              data: media.copyWith(textScaler: media.textScaler),
+              child: child,
+            );
+            return child;
+          },
+          supportedLocales: const [Locale('en', 'US'), Locale('pt', 'BR')],
+          locale: locale,
+          routerConfig: GoRouter(
+            routes: [
+              GoRoute(path: '/', builder: (context, _) => HomeScreen()),
+              GoRoute(
+                path: '/characters',
+                builder: (context, _) => Placeholder(),
+              ),
+              GoRoute(path: '/seasons', builder: (context, _) => Placeholder()),
+              GoRoute(
+                path: '/episodes',
+                builder: (context, _) => Placeholder(),
+              ),
+            ],
+          ),
         );
-        final MediaQueryData media = MediaQuery.of(context);
-        child = MediaQuery(
-          data: media.copyWith(textScaler: media.textScaler),
-          child: child,
-        );
-        return child;
       },
-      supportedLocales: const [Locale('en', 'US'), Locale('pt', 'BR')],
-      locale: const Locale('en', 'US'),
-      routerConfig: GoRouter(
-        routes: [
-          GoRoute(path: '/', builder: (context, _) => HomeScreen()),
-          GoRoute(path: '/characters', builder: (context, _) => Placeholder()),
-          GoRoute(path: '/seasons', builder: (context, _) => Placeholder()),
-          GoRoute(path: '/episodes', builder: (context, _) => Placeholder()),
-        ],
-      ),
     );
   }
 }
