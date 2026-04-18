ad5ed0069f925b66cbdd2a3d6b6465ddd3a5c26c
Enzo Santos <enzo.santos29@hotmail.com>
2026-04-18 15:56:45 -0300

    [f] feat: add responsiveness to HomeScreen

--- frontend/lib/screens/homepage.dart
+++ frontend/lib/screens/homepage.dart
@@ -3,11 +3,128 @@ import 'package:flutter_svg/svg.dart';
 import 'package:responsive_framework/responsive_framework.dart' as rf;
 import 'package:twd_hub/main.dart';
 
+class MaybeWidget extends StatelessWidget {
+  final Widget child;
+  final bool enabled;
+  final Widget Function(Widget child) builder;
+
+  const MaybeWidget({
+    super.key,
+    required this.enabled,
+    required this.builder,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) => enabled ? builder(child) : child;
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final bool scrollable = rf.ResponsiveBreakpoints.of(
+      context,
+    ).smallerThan(kDeviceDesktop);
+    final List<Widget> children = [
+      Padding(
+        padding: const EdgeInsets.all(AppSpacing.lg),
+        child: SvgPicture.network(
+          "https://upload.wikimedia.org/wikipedia/commons/e/ef/The_Walking_Dead_2010_logo.svg",
+          height: 100,
+        ),
+      ),
+      MaybeWidget(
+        enabled: !scrollable,
+        builder: (child) => Expanded(child: child),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: AppSpacing.huge),
+          child: rf.ResponsiveRowColumn(
+            layout: scrollable
+                ? rf.ResponsiveRowColumnType.COLUMN
+                : rf.ResponsiveRowColumnType.ROW,
+            children: [
+              rf.ResponsiveRowColumnItem(
+                rowFlex: 7,
+                rowFit: FlexFit.tight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    MaybeWidget(
+                      enabled: !scrollable,
+                      builder: (child) => Expanded(child: child),
+                      child: Image.network(
+                        "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/03/06/13/the-walking-dead-cast-1.jpg",
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter,
+                      ),
+                    ),
+                    SizedBox(height: AppSpacing.xxxl),
+                    Center(
+                      child: Text(
+                        "Bem-vindo ao seu guia definitivo de personagens de The "
+                        "Walking Dead!\nAqui você encontra um panorama completo "
+                        "sobre cada sobrevivente que marcou a série — desde sua "
+                        "primeira aparição até seu destino final. Consulte "
+                        "rapidamente quando e como cada personagem morreu, "
+                        "incluindo as circunstâncias principais. Veja se houve "
+                        "aparições em flashbacks e em quais episódios ocorreram. "
+                        "Acompanhe também a primeira aparição, estatísticas ao "
+                        "longo das temporadas e redescubra personagens que podem "
+                        "ter passado despercebidos.",
+                        style: TextStyle(fontSize: 28),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              rf.ResponsiveRowColumnItem(
+                child: scrollable
+                    ? const SizedBox(height: AppSpacing.xl)
+                    : const SizedBox(width: AppSpacing.huge),
+              ),
+              rf.ResponsiveRowColumnItem(
+                rowFlex: 4,
+                rowFit: FlexFit.tight,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Center(
+                        child: Text("Menu", style: TextStyle(fontSize: 64)),
+                      ),
+                      const SizedBox(height: AppSpacing.lg),
+                      Column(
+                        children: [
+                          _Button(label: "Personagens"),
+                          SizedBox(height: AppSpacing.md),
+                          _Button(label: "Temporadas"),
+                          SizedBox(height: AppSpacing.md),
+                          _Button(label: "Episódios"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(AppSpacing.lg),
+        child: Center(
+          child: Text(
+            "Criado por: github.com/ezgrs",
+            style: TextStyle(fontSize: 36),
+          ),
+        ),
+      ),
+    ];
     return Scaffold(
       body: Ink(
         decoration: const BoxDecoration(
@@ -18,105 +135,9 @@ class HomeScreen extends StatelessWidget {
             stops: [0.0, 1.0],
           ),
         ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(AppSpacing.lg),
-              child: SvgPicture.network(
-                "https://upload.wikimedia.org/wikipedia/commons/e/ef/The_Walking_Dead_2010_logo.svg",
-                height: 100,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: AppSpacing.huge),
-                child: rf.ResponsiveRowColumn(
-                  layout:
-                      rf.ResponsiveBreakpoints.of(
-                        context,
-                      ).smallerThan(kDeviceLaptop)
-                      ? rf.ResponsiveRowColumnType.COLUMN
-                      : rf.ResponsiveRowColumnType.ROW,
-                  children: [
-                    rf.ResponsiveRowColumnItem(
-                      rowFlex: 7,
-                      rowFit: FlexFit.tight,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            child: Image.network(
-                              "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/03/06/13/the-walking-dead-cast-1.jpg",
-                              fit: BoxFit.cover,
-                              alignment: Alignment.topCenter,
-                            ),
-                          ),
-                          SizedBox(height: AppSpacing.xxxl),
-                          Center(
-                            child: Text(
-                              "Bem-vindo ao seu guia definitivo de personagens de The "
-                              "Walking Dead!\nAqui você encontra um panorama completo "
-                              "sobre cada sobrevivente que marcou a série — desde sua "
-                              "primeira aparição até seu destino final. Consulte "
-                              "rapidamente quando e como cada personagem morreu, "
-                              "incluindo as circunstâncias principais. Veja se houve "
-                              "aparições em flashbacks e em quais episódios ocorreram. "
-                              "Acompanhe também a primeira aparição, estatísticas ao "
-                              "longo das temporadas e redescubra personagens que podem "
-                              "ter passado despercebidos.",
-                              style: TextStyle(fontSize: 28),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    rf.ResponsiveRowColumnItem(
-                      child: const SizedBox(width: AppSpacing.huge),
-                    ),
-                    rf.ResponsiveRowColumnItem(
-                      rowFlex: 4,
-                      rowFit: FlexFit.tight,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Center(
-                              child: Text(
-                                "Menu",
-                                style: TextStyle(fontSize: 64),
-                              ),
-                            ),
-                            const SizedBox(height: AppSpacing.lg),
-                            Column(
-                              children: [
-                                _Button(label: "Personagens"),
-                                SizedBox(height: AppSpacing.md),
-                                _Button(label: "Temporadas"),
-                                SizedBox(height: AppSpacing.md),
-                                _Button(label: "Episódios"),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(AppSpacing.lg),
-              child: Center(
-                child: Text(
-                  "Criado por: github.com/ezgrs",
-                  style: TextStyle(fontSize: 36),
-                ),
-              ),
-            ),
-          ],
-        ),
+        child: scrollable
+            ? ListView(children: children)
+            : Column(children: children),
       ),
     );
   }
