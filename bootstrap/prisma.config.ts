8d09cf950707e3e79ee82d411f534565176bc027
Enzo Santos <enzo.santos29@hotmail.com>
2026-08-29 18:37:57 -0300

    [bootstrap] fix: remove remaning extra spaces on Prisma's connection string

--- bootstrap/prisma.config.ts
+++ bootstrap/prisma.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
             `postgresql://` +
             `${process.env['POSTGRES_USERNAME']}:` +
             `${process.env['POSTGRES_PASSWORD']}@` +
-            `${process.env['POSTGRES_HOST']}: ` +
-            `${process.env['POSTGRES_PORT']}/ ` +
+            `${process.env['POSTGRES_HOST']}:` +
+            `${process.env['POSTGRES_PORT']}/` +
             `${process.env['POSTGRES_DATABASE']}`,
     },
 })
